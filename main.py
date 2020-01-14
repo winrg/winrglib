a57b00ca03905df3f1651e18781f94c73779d714
winrg <julianshipp4@gmail.com>
2020-01-14 18:47:18 +0000

    more stuff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import curses
-import winrglib
 
 
 #init curses
@@ -15,20 +14,21 @@ stdscr.keypad(True)
 curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
 stdscr.bkgd(curses.color_pair(1))
 stdscr.timeout(42)
+lines = [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, ]
 
 x = 1
 y = 1
-options = [['upgrade 1', '10', 490, 'get1'], ['upgrade 2', '20', 500, 'get2'], ['upgrade 3', '30', 510, 'get3'], ['prestige', '100']]
-costs = [10,20,30,100]
-outputs = ['get1', 'get2', 'get3' 'getp']
-maxheight = 20
+options = [['upgrade 1', '10', 490, 'get1'], ['upgrade 2', '20', 500, 'get2'], ['upgrade 3', '30', 510, 'get3'], ['prestige', '100', 80, 'getp']]
+height = 20
 width = 20
 exitkey = 120
 
 usewidth = width - 2
 x = 0
 for option in options:
-  x += 1
-  thefrigginlengthofthelinesspaces = usewidth - len(option) - len(str(costs[x]))
+  thefrigginlengthofthelinesspaces = usewidth - len(option[1]) - len(option[0])
   if thefrigginlengthofthelinesspaces >= 5:
-    lines = [option + ' ' * thefrigginlengthofthelinesspaces + str(costs)]
+    lines[x] = '|' + option[0] + ' ' * thefrigginlengthofthelinesspaces + option[1] + '|'
+stdscr.addstr(1, 1, '+' + '-' * usewidth + '+')
+stdscr.addstr(2, 1, lines[0])
+stdscr.refresh()
